55aac569f038ec0e17dc02d70df824d0c03623f7
Siddiq Umer <hafiz.siddiq2k17@gmail.com>
2022-11-01 15:28:51 +0500

    Revert "UI branch"

--- resources/views/Admin/user/index.blade.php
+++ resources/views/Admin/user/index.blade.php
@@ -1,110 +1,140 @@
 @extends('dashboard')
 @section('content')
+
     <div class="card p-3 table-card">
-       <div class="table-responsive">
-        <table class="table table-hover">
-            <thead class="bg-primary text-white header-border text-center">
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">User Type</th>
-                <th scope="col">Properties </th>
-                <th scope="col">Bids</th>
-                <th scope="col">Markets</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody class="text-center">
-              <tr>
-                <th>1</th>
-                <td>abc</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>abcd</td>
-                <td class="">
-                  <div class="d-flex justify-content-center">
-                    <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#addusermodal">add</button>
-                    <button class="btn btn-info btn-sm ml-1">view</button>
-                      <button class="btn btn-danger btn-sm ml-1">delele</button>
-                    <button class="btn btn-warning btn-sm ml-1" data-bs-toggle="modal" data-bs-target="#editusermodal">edit</button>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
+        <div class="d-flex justify-content-end mb-3">
+            <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#addusermodal">Add User</button>
+        </div>
+       <div class="table-responsive" >
+        <table class="table table-hover" id="user-data-table" width="100%">
+            <div>
+                <thead class="bg-primary text-white header-border text-center">
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Email </th>
+                      <th width="30%" scope="col">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody class="text-center">
+                    {{-- <tr>
+                      <th>1</th>
+                      <td>Otto</td>
+                      <td>abcd</td>
+                      <td class="">
+                        <div class="d-flex justify-content-center">
+                          <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#addusermodal">add</button>
+                          <button class="btn btn-info btn-sm ml-1">view</button>
+                            <button class="btn btn-danger btn-sm ml-1">delele</button>
+                          <button class="btn btn-warning btn-sm ml-1" data-bs-toggle="modal" data-bs-target="#editusermodal">edit</button>
+                        </div>
+                      </td>
+                    </tr> --}}
+                  </tbody>
+            </div>
+
           </table>
        </div>
     </div>
-  
-  <!-- Modal -->
-  <div class="modal fade" id="addusermodal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title" id="staticBackdropLabel">Add user</h4>
-        </div>
-        <div class="modal-body">
-          <div>
-            <form action="">
-                <div class="mt-4 mb-4">
-                    <div class=" input-group-md">
-                        <input type="text" required class="form-control" placeholder="Enter User Type" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                    <div class=" input-group-md mt-3">
-                        <input type="text" required class="form-control" placeholder="Enter Properties" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                      <div class="input-group-md mt-3">
-                        <input type="text" required class="form-control" placeholder="Enter Bids" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                      <div class="input-group-md mt-3">
-                        <input type="text" required class="form-control" placeholder="Enter Markets" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                </div>
-               </form>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary btn-md" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary btn-md">Add</button>
-        </div>
-      </div>
-    </div>
-  </div>
+
+  @include('Admin.user.create')
+  @include('Admin.user.edit')
 
 
-  <div class="modal fade" id="editusermodal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title" id="staticBackdropLabel">Edit user</h4>
-        </div>
-        <div class="modal-body">
-          <div>
-            <form action="">
-                <div class="mt-4 mb-4">
-                    <div class=" input-group-md">
-                        <input type="text" required class="form-control" placeholder="Enter User Type" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                    <div class=" input-group-md mt-3">
-                        <input type="text" required class="form-control" placeholder="Enter Properties" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                      <div class="input-group-md mt-3">
-                        <input type="text" required class="form-control" placeholder="Enter Bids" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                      <div class="input-group-md mt-3">
-                        <input type="text" required class="form-control" placeholder="Enter Markets" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                      </div>
-                </div>
-               </form>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary btn-md" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary btn-md">Edit</button>
-        </div>
-      </div>
-    </div>
-  </div>
+
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+<script>
+    $(document).ready(function () {
+
+        var table =  $('#user-data-table').DataTable({
+
+            "bSort": true,
+            "paging": true,
+            "scrollCollapse": true,
+            "lengthChange": true,
+            "pageLength": 20,
+            "responsive": true,
+            "processing": false,
+            "serverSide": true,
+            "lengthMenu": [ 5, 10, 20, 30, 50 ],
+            "order": [[ 0, "asc" ]],
+
+
+            "ajax":{
+                    "url": "{{ route('user-data') }}",
+                    "dataType": "json",
+                    "type": "POST",
+                    "data":{ _token: "{{csrf_token()}}"}
+                },
+
+            "columns": [
+
+                { "data": "id" },
+                { "data": "name" },
+                { "data": "email" },
+                { "data": "action" , orderable: false, searchable: false }
+            ]
+
+        });
+
+});
+
+
+
+
+
+
+
+
+
+    $(document).on('click', '.delete-user', function(){
+            var id = $(this).data('id');
+alert(id);
+
+            swal({
+                title: "Are you sure?",
+                text: "Once deleted, you will not be able to recover this imaginary file!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+                })
+                .then((willDelete) => {
+                if (willDelete) {
+
+                var url = "{{ route('users.destroy',':id') }}";
+                url = url.replace(':id', id);
+
+                var token = "{{ csrf_token() }}";
+
+                $.ajax({
+                    type: 'Delete',
+                        url: url,
+                        data: {'_token': token, '_method': 'DELETE'},
+                        success: function (response) {
+
+                            $('#user-data-table').DataTable().ajax.reload();
+                            swal("Poof! User has been deleted!", {
+                                icon: "success",
+                                timer: 1000,
+                            });
+                        }
+                });
+
+
+
+
+                } else {
+                    swal("Your Category is safe!");
+                }
+            });
+
+
+
+
+
+    });
+</script>
 
 @endsection
